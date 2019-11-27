b44ffa55eddfccbf8d46f6e10efbde62b52668c2
andika <rosa.andika@live.com>
2019-11-27 10:00:55 +0700

    Crawler Add Exception and out file name

--- AMNSpeedTesting/AMNSpeedTesting/Program.cs
+++ AMNSpeedTesting/AMNSpeedTesting/Program.cs
@@ -24,7 +24,7 @@ namespace AMNSpeedTesting
 
                 Console.WriteLine("Start Crawler");
                 String patternOut = String.Format("{0:yyyyMMddHHmmss}", DateTime.Now);
-                String path = Path.Combine(System.AppDomain.CurrentDomain.BaseDirectory, $"output_{patternOut}.csv");
+                String path = Path.Combine(System.AppDomain.CurrentDomain.BaseDirectory, $"output_{t.UserName}_{patternOut}.csv");
                 if (File.Exists(path))
                 {
                     File.Delete(path);
